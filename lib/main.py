f1c6d45cae86f4931fe986a84defffe34950e0f4
wanjiku8 <wanjikuf141@gmail.com>
2025-02-17 08:13:35 -0800

    commit

---
+++ lib/main.py
@@ -0,0 +1,14 @@
+# main.py
+from song import Song  # Import the Song class
+
+
+song1 = Song("99 Problems", "Jay-Z", "Rap")
+song2 = Song("Single Ladies", "Beyonce", "Pop")
+song3 = Song("God's Plan", "Drake", "Rap")
+
+
+print(Song.count)             
+print(Song.genres)          
+print(Song.artists)           
+print(Song.genre_count)      
+print(Song.artist_count)     
